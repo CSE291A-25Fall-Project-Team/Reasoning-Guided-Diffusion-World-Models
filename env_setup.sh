9ec63036870ef6c594858a08409530e81effea33
ShengxiangJi <jishengxiangzs@gmail.com>
2025-11-16 06:26:33 +0800

    update .gitignore and env_setup

--- env_setup.sh
+++ env_setup.sh
@@ -1,3 +1,5 @@
+source /etc/network_turbo
+
 cd packages
 git clone -b robocasa https://github.com/ARISE-Initiative/robomimic && pip install -e robomimic 
 git clone https://github.com/ARISE-Initiative/robosuite && pip install -e robosuite 
@@ -5,7 +7,7 @@ git clone https://github.com/robocasa/robocasa && pip install -e robocasa
 
 # python robocasa/robocasa/scripts/download_kitchen_assets.py && \
 rm -rf robocasa/robocasa/models/assets
-mkdir -p robocasa/robocasa/models/assets && unzip asset.zip -d robocasa/robocasa/models/assets
+mkdir -p robocasa/robocasa/models/assets && unzip /root/autodl-tmp/assets.zip -d robocasa/robocasa/models/assets
 python robocasa/robocasa/scripts/setup_macros.py
 
 cd ..
